46239e02e6ab9e68fb19a62c4b0965f853164656
Sam Thompson <samthompson292@gmail.com>
2021-02-03 14:35:00 +0000

    remove else

--- yhs/settings.py
+++ yhs/settings.py
@@ -128,8 +128,8 @@ MEDIA_URL = '/media/'
 
 if DEBUG:   
     STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
-else:
-    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
