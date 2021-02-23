cc9d950e976b8e75ecef2f5873551b67b5c5e0c5
Sam Thompson <samthompson292@gmail.com>
2021-02-23 17:14:38 +0000

    redesign front page

--- yhs/settings.py
+++ yhs/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = '*5k38!j6wh7w51qy@)(5@5u@zq_^efezj+ddch#dwb+5gz3uyd'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['206.189.17.4']
+ALLOWED_HOSTS = ['206.189.17.4', '127.0.0.1']
 
 
 # Application definition
@@ -127,9 +127,9 @@ USE_TZ = True
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 
-# STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
